890d3d83706ff0c57d6228cb56764de203e32e9b
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2023-09-07 13:40:59 +0200

    update init

--- sopa/io/explorer/__init__.py
+++ sopa/io/explorer/__init__.py
@@ -1,5 +1,6 @@
 from .categories import write_groups
 from .experiment import write_experiment
+from .features import write_features
 from .ome import write_multiscale, write_np_ome_tif, write_ome_tif
 from .polygons import write_polygons
 from .transcripts import write_transcripts
