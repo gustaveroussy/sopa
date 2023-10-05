7024fe9d8c75db9d366b34ec00a44794bfbada1b
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2023-10-05 17:48:58 +0200

    lazy writing images: working

--- sopa/io/explorer/images.py
+++ sopa/io/explorer/images.py
@@ -7,85 +7,86 @@ import xarray as xr
 from multiscale_spatial_image import MultiscaleSpatialImage, to_multiscale
 from spatial_image import SpatialImage
 
+from ...utils.image import scale_dtype
 from ._constants import image_metadata
 
 log = logging.getLogger(__name__)
 
 
-def _astype_uint8(arr: np.ndarray) -> np.ndarray:
-    assert np.issubdtype(
-        arr.dtype, np.integer
-    ), f"The image dtype has to be an integer dtype. Found {arr.dtype}"
+class MultiscaleImageWriter:
+    photometric = "minisblack"
+    compression = "jpeg2000"
+    resolutionunit = "CENTIMETER"
+    dtype = np.uint8
 
-    if arr.dtype == np.uint8:
-        return arr
+    def __init__(self, image: MultiscaleSpatialImage, tile_width: int, pixelsize: float):
+        self.image = image
+        self.tile_width = tile_width
+        self.pixelsize = pixelsize
 
-    factor = np.iinfo(np.uint8).max / np.iinfo(arr.dtype).max
-    return (arr * factor).astype(np.uint8)
+        self.scale_names = list(image.children)
+        self.channel_names = list(map(str, image[self.scale_names[0]].c.values))
+        self.metadata = image_metadata(self.channel_names, pixelsize)
 
+    def _get_tiles(self, xarr: xr.DataArray):
+        for c in range(xarr.shape[0]):
+            for index_y in range(ceil(xarr.shape[1] / self.tile_width)):
+                for index_x in range(ceil(xarr.shape[2] / self.tile_width)):
+                    tile = xarr[
+                        c,
+                        self.tile_width * index_y : self.tile_width * (index_y + 1),
+                        self.tile_width * index_x : self.tile_width * (index_x + 1),
+                    ].values
+                    yield scale_dtype(tile, self.dtype)
 
-def _get_tiles(xarr: xr.DataArray, tile_width: int):
-    log.info(f"   Image of shape {xarr.shape}")
-    for c in range(xarr.shape[0]):
-        for index_x in range(ceil(xarr.shape[2] / tile_width)):
-            for index_y in range(ceil(xarr.shape[1] / tile_width)):
-                tile = xarr[
-                    c,
-                    tile_width * index_x : tile_width * (index_x + 1),
-                    tile_width * index_y : tile_width * (index_y + 1),
-                ].values
-                yield _astype_uint8(tile)
+    def _write_image_level(self, tif: tf.TiffWriter, scale_index: int, **kwargs):
+        xarr: xr.DataArray = next(iter(self.image[self.scale_names[scale_index]].values()))
+        resolution = 1e4 * 2**scale_index / self.pixelsize
 
+        data = self._get_tiles(xarr) if self.lazy else scale_dtype(xarr.values, self.dtype)
 
-def _write_image_level(
-    tif: tf.TiffWriter, xarr: xr.DataArray, tile_width, metadata, resolution, **kwargs
-):
-    tif.write(
-        _get_tiles(xarr, tile_width),
-        tile=(tile_width, tile_width),
-        resolution=(resolution, resolution),
-        metadata=metadata,
-        shape=xarr.shape,
-        dtype=xarr.dtype,
-        photometric="minisblack",
-        compression="jpeg2000",
-        resolutionunit="CENTIMETER",
-        **kwargs,
-    )
+        log.info(f"   > Image of shape {xarr.shape}")
+        tif.write(
+            data,
+            tile=(self.tile_width, self.tile_width),
+            resolution=(resolution, resolution),
+            metadata=self.metadata,
+            shape=xarr.shape,
+            dtype=xarr.dtype,
+            photometric=self.photometric,
+            compression=self.compression,
+            resolutionunit=self.resolutionunit,
+            **kwargs,
+        )
+
+    def __len__(self):
+        return len(self.scale_names)
+
+    def write(self, path, lazy=True):
+        self.lazy = lazy
+        log.info(f"Writing multiscale image (lazy-mode={self.lazy})")
+
+        with tf.TiffWriter(path, bigtiff=True) as tif:
+            self._write_image_level(tif, 0, subifds=len(self) - 1)
+
+            for i in range(1, len(self)):
+                self._write_image_level(tif, i, subfiletype=1)
 
 
 def write_image(
     path: str,
-    image: SpatialImage,
-    pixelsize: float = 0.2125,
+    image: SpatialImage | np.ndarray,
+    lazy: bool = True,
     tile_width: int = 1024,
+    n_subscales: int = 5,
+    pixelsize: float = 0.2125,
 ):
-    log.info("Writing multiscale image")
+    if isinstance(image, np.ndarray):
+        assert len(image.shape) == 3, "Can only write channels with shape (C,Y,X)"
+        log.info(f"Converting image of shape {image.shape} into a SpatialImage (with dims: C,Y,X)")
+        image = SpatialImage(image, dims=["c", "y", "x"], name="image")
 
-    image_key = image.name
-    image: MultiscaleSpatialImage = to_multiscale(image, [2, 2, 2, 2, 2])
+    image: MultiscaleSpatialImage = to_multiscale(image, [2] * n_subscales)
 
-    scale_names = list(image.children)
-    channel_names = list(map(str, image[scale_names[0]].c.values))
-
-    metadata = image_metadata(channel_names, pixelsize)
-
-    with tf.TiffWriter(path, bigtiff=True) as tif:
-        _write_image_level(
-            tif,
-            image[scale_names[0]][image_key],
-            tile_width,
-            metadata,
-            1e4 / pixelsize,
-            subifds=len(scale_names) - 1,
-        )
-
-        for i, scale in enumerate(scale_names[1:]):
-            _write_image_level(
-                tif,
-                image[scale][image_key],
-                tile_width,
-                metadata,
-                1e4 * 2 ** (i + 1) / pixelsize,
-                subfiletype=1,
-            )
+    image_writer = MultiscaleImageWriter(image, pixelsize=pixelsize, tile_width=tile_width)
+    image_writer.write(path, lazy=lazy)
--- sopa/utils/image.py
+++ sopa/utils/image.py
@@ -6,9 +6,7 @@ import xarray as xr
 
 def resize(xarr: xr.DataArray, scale_factor: float) -> da.Array:
     resize_dims = [dim in ["x", "y"] for dim in xarr.dims]
-    transform = np.diag(
-        [scale_factor if resize_dim else 1 for resize_dim in resize_dims]
-    )
+    transform = np.diag([scale_factor if resize_dim else 1 for resize_dim in resize_dims])
     output_shape = [
         size // scale_factor if resize_dim else size
         for size, resize_dim in zip(xarr.shape, resize_dims)
@@ -17,3 +15,18 @@ def resize(xarr: xr.DataArray, scale_factor: float) -> da.Array:
     return dask_image.ndinterp.affine_transform(
         xarr.data, matrix=transform, output_shape=output_shape
     )
+
+
+def _check_integer_dtype(dtype: np.dtype):
+    assert np.issubdtype(dtype, np.integer), f"Expecting integer dtype, but found {dtype}"
+
+
+def scale_dtype(arr: np.ndarray, dtype: np.dtype) -> np.ndarray:
+    _check_integer_dtype(arr.dtype)
+    _check_integer_dtype(dtype)
+
+    if arr.dtype == dtype:
+        return arr
+
+    factor = np.iinfo(dtype).max / np.iinfo(arr.dtype).max
+    return (arr * factor).astype(dtype)
