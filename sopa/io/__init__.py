99372e2669fd790ea829ae812bf7256e7563f467
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2023-10-17 17:01:39 +0200

    merscope: no need for cellpose

--- sopa/io/__init__.py
+++ sopa/io/__init__.py
@@ -1,5 +1,6 @@
+from spatialdata_io import xenium, cosmx
+
 from .imaging import qptiff
 from .explorer import write_explorer
 from .standardize import write_standardized
 from .transcriptomics import merscope
-from spatialdata_io import xenium, cosmx
