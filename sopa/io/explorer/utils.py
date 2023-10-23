4c1af6953b29f5729c6b6817597e6a5517792335
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2023-10-23 16:43:47 +0200

    is_dir opposed logic

--- sopa/io/explorer/utils.py
+++ sopa/io/explorer/utils.py
@@ -4,7 +4,7 @@ from pathlib import Path
 def explorer_file_path(path: str, filename: str, is_dir: bool):
     path: Path = Path(path)
 
-    if not is_dir:
+    if is_dir:
         path = path / filename
 
     return path
