1709bed74e018c15acf215d2ff74b300a0730ed8
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2023-10-24 15:53:01 +0200

    fluo: ok if annot after baysor

--- sopa/annotation/fluorescence.py
+++ sopa/annotation/fluorescence.py
@@ -1,6 +1,7 @@
 import logging
 
 import numpy as np
+import pandas as pd
 from anndata import AnnData
 
 from .._constants import SopaKeys
@@ -9,12 +10,17 @@ log = logging.getLogger(__name__)
 
 
 def higher_z_score(adata: AnnData, marker_cell_dict: dict, key: str = "cell_type"):
-    scaled = np.arcsinh(adata.X / (5 * np.quantile(adata.X, 0.2, axis=0)))
+    if SopaKeys.INTENSITIES_OBSM in adata.obsm:
+        df = adata.obsm[SopaKeys.INTENSITIES_OBSM]
+    else:
+        df = adata.to_df()
 
-    adata.layers[SopaKeys.Z_SCORES] = (scaled - scaled.mean(0)) / scaled.std(0)
+    scaled = np.arcsinh(df / (5 * np.quantile(df, 0.2, axis=0)))
+
+    adata.obsm[SopaKeys.Z_SCORES] = (scaled - scaled.mean(0)) / scaled.std(0)
 
     markers, cell_types = list(marker_cell_dict.keys()), np.array(list(marker_cell_dict.values()))
-    ct_indices = adata[:, markers].layers[SopaKeys.Z_SCORES].argmax(1)
+    ct_indices = adata.obsm[SopaKeys.Z_SCORES][markers].values.argmax(1)
 
     adata.obs[key] = cell_types[ct_indices]
 
