170573f64eb422a46c37093c2da084802383e1ac
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2023-10-24 14:43:35 +0200

    report engine

---
+++ sopa/io/report/__init__.py
@@ -0,0 +1,1 @@
+from .generate import write_report
--- sopa/io/report/engine.py
+++ sopa/io/report/engine.py
@@ -1,9 +1,6 @@
 from pathlib import Path
 from typing import Optional
 
-import spatialdata
-from spatialdata import SpatialData
-
 
 class Renderable:
     @property
@@ -257,32 +254,3 @@ class Root(Renderable):
     </body>
     </html>
     """
-
-
-def write_report(path: str, sdata):
-    section1 = Section(
-        "General",
-        [
-            SubSection(
-                "SpatialData information",
-                [
-                    Paragraph(
-                        f"Sopa is using <a href='https://spatialdata.scverse.org/en/latest/'>SpatialData</a> under the hood. This is how the object looks like:"
-                    ),
-                    CodeBlock(str(sdata)),
-                ],
-            )
-        ],
-    )
-    section2 = Section("Section 2!", [SubSection("subsection12"), SubSection("subsection22")])
-    section3 = Section("Section 3!", [SubSection("subsection13"), SubSection("subsection23")])
-    section4 = Section("Section 4!", [SubSection("subsection14"), SubSection("subsection24")])
-    section5 = Section("Section 5!", [SubSection("subsection15"), SubSection("subsection25")])
-
-    root = Root([section1, section2, section3, section4, section5])
-    root.write(path)
-
-
-# sdata = spatialdata.read_zarr("/Volumes/T7_Quentin/data/vizgen/test.zarr")
-sdata = None
-write_report("test.html", sdata)
