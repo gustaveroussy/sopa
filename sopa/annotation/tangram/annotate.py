83eb4eda2e32cdc55a8dbd4495324b914857f767
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2023-10-27 18:00:45 +0200

    ready to test tangram

--- sopa/annotation/tangram/annotate.py
+++ sopa/annotation/tangram/annotate.py
@@ -12,12 +12,12 @@ from spatialdata import SpatialData
 log = logging.getLogger(__name__)
 
 
-def annotate(sdata: SpatialData, adata_sc: AnnData, bag_size: int = 10_000, **kwargs):
+def tangram_annotate(
+    sdata: SpatialData, adata_sc: AnnData, cell_type_key: str, bag_size: int = 10_000, **kwargs
+):
     ad_sp = sdata.table
 
-    MultiLevelAnnotation(ad_sp, adata_sc, bag_size, **kwargs).run()
-
-    sdata.table = ...  # TODO: update table
+    MultiLevelAnnotation(ad_sp, adata_sc, cell_type_key, bag_size, **kwargs).run()
 
 
 class MultiLevelAnnotation:
@@ -25,12 +25,14 @@ class MultiLevelAnnotation:
         self,
         ad_sp: AnnData,
         ad_sc: AnnData,
+        cell_type_key: str,
         bag_size: int,
         max_obs_reference: int = 10_000,
         clip_percentile: float = 0.95,
     ):
         self.ad_sp = ad_sp
         self.ad_sc = ad_sc
+        self.cell_type_key = cell_type_key
         self.bag_size = bag_size
         self.max_obs_reference = max_obs_reference
         self.clip_percentile = clip_percentile
@@ -38,30 +40,29 @@ class MultiLevelAnnotation:
         self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
         log.info("Using device:", self.device)
 
-    def level_name(self, i):
-        return f"ct_level{i}"
+    def _level_suffix(self, level: int) -> str:
+        return "" if level == 0 else f"_level{level}"
 
-    def umap_name(self, i):
-        return f"X_umap_level{i}"
+    def level_obs_key(self, level: int) -> str:
+        return f"{self.cell_type_key}{self._level_suffix(level)}"
 
-    def get_obsm_key(self, level):
-        return f"tangram_{level}_pred"
+    def probs_key(self, level: int) -> str:
+        return f"tangram_pred{self._level_suffix(level)}"
 
+    @property
     def levels(self):
         i = 0
-        while self.level_name(i) in self.ad_sc.obs.columns:
-            yield (i, self.level_name(i))
+        while self.level_obs_key(i) in self.ad_sc.obs.columns:
             i += 1
+        return i
 
-    def split_indices(self, indices, n_splits):
-        if indices is None:
-            indices = self.ad_sp.obs_names
+    def split_indices(self, indices: pd.Series, n_splits: int) -> list(np.ndarray):
         indices = indices.values.copy()
         np.random.shuffle(indices)
         return np.array_split(indices, n_splits)
 
-    def init_obsm(self, level):
-        self.ad_sp.obsm[self.get_obsm_key(level)] = pd.DataFrame(
+    def init_obsm(self, level: int):
+        self.ad_sp.obsm[self.probs_key(level)] = pd.DataFrame(
             0.0, index=self.ad_sp.obs_names, columns=self.ad_sc.obs[level].unique()
         )
 
@@ -72,7 +73,7 @@ class MultiLevelAnnotation:
         df = (df - df.min()) / (df.max() - df.min())
         return df.idxmax(1)
 
-    def pp_adata(self, ad_sp_: AnnData, split) -> AnnData:
+    def pp_adata(self, ad_sp_: AnnData, split: np.ndarray) -> AnnData:
         "Copied and updated from Tangram pp_adatas()"
 
         ad_sp_split = ad_sp_[split].copy()
@@ -90,41 +91,37 @@ class MultiLevelAnnotation:
         return ad_sp_split
 
     def run(self):
-        for i, level in self.levels():
-            print(f"--- Running on {level}")
-            self.init_obsm(level)
-            self.ad_sp.obs[level] = np.nan
+        for level in range(self.levels):
+            log.info(f"> Running on level {level}")
 
-            if i == 0:
-                self.run_group(level, i)
+            self.init_obsm(level)
+            self.ad_sp.obs[self.level_obs_key(level)] = np.nan
+
+            if level == 0:
+                self.run_group()
                 continue
 
-            self.ad_sp.obsm[self.umap_name(i)] = pd.DataFrame(
-                self.ad_sp.obsm["X_umap"].copy(),
-                index=self.ad_sp.obs_names,
-                columns=["0", "1"],
-            )
-
-            previous_level = self.level_name(i - 1)
-            self.ad_sp.obs[level] = self.ad_sp.obs[previous_level]
-            groups = self.ad_sc.obs.groupby(previous_level)
+            previous_key = self.level_obs_key(level - 1)
+            obs_key = self.level_obs_key(level)
+            self.ad_sp.obs[obs_key] = self.ad_sp.obs[previous_key]
+            groups = self.ad_sc.obs.groupby(previous_key)
             for ct in groups.groups.keys():
-                group = groups.get_group(ct)
-                indices_sp = self.ad_sp.obs_names[self.ad_sp.obs[previous_level] == ct]
+                group: pd.DataFrame = groups.get_group(ct)
+                indices_sp = self.ad_sp.obs_names[self.ad_sp.obs[previous_key] == ct]
 
-                sub_cts = group[level].unique()
+                sub_cts = group[obs_key].unique()
                 if len(sub_cts) == 1:
-                    self.ad_sp.obsm[self.get_obsm_key(level)].loc[indices_sp, sub_cts[0]] = 1
+                    self.ad_sp.obsm[self.probs_key(level)].loc[indices_sp, sub_cts[0]] = 1
                     continue
 
-                print(f"\n[Cell type {ct}]")
-                self.run_group(level, i, indices_sp, group.index)
+                log.info(f"\n[Cell type {ct}]")
+                self.run_group(level, indices_sp, group.index)
 
-        print("\nDone")
+        log.info("\nFinished running Tangram")
 
-    def run_group(self, level, level_index, indices_sp=None, indices_sc=None):
+    def run_group(self, level: int = 0, indices_sp=None, indices_sc=None):
         if indices_sp is not None and len(indices_sp) == 0:
-            print("No cell annotated in the upper level...")
+            log.warn("No cell annotated in the upper level...")
             return
 
         indices_sp = self.ad_sp.obs_names if indices_sp is None else indices_sp
@@ -134,20 +131,18 @@ class MultiLevelAnnotation:
         ad_sc_ = self.ad_sc[indices_sc].copy()
 
         if ad_sc_.n_obs >= self.max_obs_reference:
-            print(f"Subsampling reference to {self.max_obs_reference} cells...")
+            log.info(f"Subsampling reference to {self.max_obs_reference} cells...")
             sc.pp.subsample(ad_sc_, n_obs=self.max_obs_reference)
 
-        print(f"(N_spatial={ad_sp_.n_obs}, N_ref={ad_sc_.n_obs})\n")
-
-        n_splits = math.ceil(ad_sp_.n_obs / self.bag_size)
-        obsm_key = self.get_obsm_key(level)
+        log.info(f"(n_obs_spatial={ad_sp_.n_obs}, n_obs_ref={ad_sc_.n_obs})\n")
 
         if not self.can_run(ad_sp_, ad_sc_):
-            print("No annotations at this level")
+            log.info("No annotations at this level")
             return
 
+        n_splits = math.ceil(ad_sp_.n_obs / self.bag_size)
         for i, split in enumerate(self.split_indices(indices_sp, n_splits)):
-            print(f"> Split {i + 1} / {n_splits}")
+            log.info(f"   > Split {i + 1} / {n_splits}")
 
             ad_sp_split = self.pp_adata(ad_sp_, split)
 
@@ -157,14 +152,13 @@ class MultiLevelAnnotation:
                 device=self.device,
             )
 
-            print("Getting annotations...")
-            tg.project_cell_annotations(ad_map, ad_sp_split, annotation=level)
+            tg.project_cell_annotations(ad_map, ad_sp_split, annotation=self.level_obs_key(level))
+
             res = ad_sp_split.obsm["tangram_ct_pred"]
+            self.ad_sp.obsm[self.probs_key(level)].loc[split, res.columns] = res
 
-            self.ad_sp.obsm[obsm_key].loc[split, res.columns] = res
-
-        df_group = self.ad_sp.obsm[obsm_key].loc[indices_sp]
-        self.ad_sp.obs.loc[indices_sp, level] = self.get_hard_labels(df_group)
+        df_group = self.ad_sp.obsm[self.probs_key(level)].loc[indices_sp]
+        self.ad_sp.obs.loc[indices_sp, self.level_obs_key(level)] = self.get_hard_labels(df_group)
 
     def can_run(self, ad_sp_: AnnData, ad_sc_: AnnData, min_obs: int = 10) -> bool:
         if ad_sp_.n_obs < min_obs:
--- sopa/cli/annotate.py
+++ sopa/cli/annotate.py
@@ -28,7 +28,19 @@ def fluorescence(
 @app_annotate.command()
 def tangram(
     sdata_path: str,
-    reference_path: str,
-    key: str = "cell_type",
+    sc_reference_path: str = option,
+    cell_type_key: str = "cell_type",
+    bag_size: int = 10_000,
 ):
-    ...  # TODO
+    from pathlib import Path
+
+    import anndata
+
+    from sopa.annotation.tangram.annotate import tangram_annotate
+    from sopa.io.standardize import read_zarr_standardized
+
+    sdata = read_zarr_standardized(sdata_path)
+    adata_sc = anndata.read(sc_reference_path)
+
+    tangram_annotate(sdata, adata_sc, cell_type_key, bag_size=bag_size)
+    sdata.table.write_zarr(Path(sdata_path) / "table" / "table")
