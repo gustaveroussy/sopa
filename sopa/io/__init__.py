857356058dd4d819a73f8dcae6f3f745ffcfed7c
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2023-11-08 11:47:08 +0100

    add macsima

--- sopa/io/__init__.py
+++ sopa/io/__init__.py
@@ -1,8 +1,8 @@
 from spatialdata_io import xenium, cosmx
 
-from .imaging import qptiff
+from .imaging import qptiff, macsima
 from .explorer import write_explorer
 from .standardize import write_standardized
 from .transcriptomics import merscope
 
-from ..utils.data import blobs
+from ..utils.data import blobs, uniform
--- sopa/io/imaging.py
+++ sopa/io/imaging.py
@@ -3,7 +3,9 @@ import re
 from pathlib import Path
 
 import dask.array as da
+import pandas as pd
 import tifffile as tf
+from dask_image.imread import imread
 from spatialdata import SpatialData
 from spatialdata.models import Image2DModel
 from spatialdata.transformations import Identity
@@ -11,8 +13,69 @@ from spatialdata.transformations import Identity
 log = logging.getLogger(__name__)
 
 
-def get_channel_name(description):
+def _deduplicate_names(df):
+    is_duplicated = df[0].duplicated(keep=False)
+    df.loc[is_duplicated, 0] += " (" + df.loc[is_duplicated, 1] + ")"
+    return df[0].values
+
+
+def _parse_name_macsima(file):
+    index = file.name[:3]
+    match = re.search(r"_A-(.*?)_C-", file.name)
+    if match:
+        antibody = match.group(1)
+        channel = re.search(r"_C-(.*?)\.ome\.tif", file.name).group(1)
+        uid = f"{channel}-{index}"
+    else:
+        antibody = re.search(r"_A-(.*?)\.ome\.tif", file.name).group(1)
+        uid = index
+    return [antibody, uid]
+
+
+def _get_channel_names_macsima(files):
+    df_antibodies = pd.DataFrame([_parse_name_macsima(file) for file in files])
+    return _deduplicate_names(df_antibodies)
+
+
+def macsima(
+    path: Path, image_models_kwargs: dict | None = None, imread_kwargs: dict | None = None
+) -> SpatialData:
+    image_models_kwargs = {} if image_models_kwargs is None else image_models_kwargs
+    if "chunks" not in image_models_kwargs:
+        image_models_kwargs["chunks"] = (1, 4096, 4096)
+    imread_kwargs = {} if imread_kwargs is None else imread_kwargs
+
+    files = [file for file in Path(path).iterdir() if file.suffix == ".tif"]
+
+    names = _get_channel_names_macsima(files)
+    image = da.concatenate(
+        [imread(file, **imread_kwargs) for file in files],
+        axis=0,
+    )
+
+    log.info(f"Found channel names {names}")
+
+    image_name = Path(path).absolute().stem
+    image = Image2DModel.parse(
+        image,
+        dims=("c", "y", "x"),
+        transformations={"pixels": Identity()},
+        c_coords=names,
+        **image_models_kwargs,
+    )
+
+    return SpatialData(images={image_name: image})
+
+
+def _get_channel_name_qptiff(description):
     return re.search(r"<Name>(.*?)</Name>", description).group(1)
+
+
+def _get_channel_names_qptiff(page_series):
+    df_names = pd.DataFrame(
+        [[_get_channel_name_qptiff(page.description), str(i)] for i, page in enumerate(page_series)]
+    )
+    return _deduplicate_names(df_names)
 
 
 def qptiff(
@@ -24,7 +87,7 @@ def qptiff(
 
     with tf.TiffFile(path) as tif:
         page_series = tif.series[0]
-        names = [get_channel_name(page.description) for page in page_series]
+        names = _get_channel_names_qptiff(page_series)
 
         log.info(f"Found channel names {names}")
 
