cb68229045d0a8107608bf70c8cad6a485978f2d
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2023-11-17 11:46:01 +0100

    rename domain -> niche

--- sopa/stats/__init__.py
+++ sopa/stats/__init__.py
@@ -1,3 +1,3 @@
 from ._build import spatial_neighbors
-from .morpho import geometrize_domains, domains_geometry_stats
+from .morpho import geometrize_niches, niches_geometry_stats
 from .distance import cells_to_groups, mean_distance
--- sopa/stats/morpho.py
+++ sopa/stats/morpho.py
@@ -12,13 +12,13 @@ from ._graph import Component
 log = logging.getLogger(__name__)
 
 
-def geometrize_domains(adata: AnnData, domain_key: str) -> gpd.GeoDataFrame:
+def geometrize_niches(adata: AnnData, niche_key: str) -> gpd.GeoDataFrame:
     _check_has_delaunay(adata)
-    data = {"geometry": [], domain_key: []}
+    data = {"geometry": [], niche_key: []}
 
     delaunay = Delaunay(adata.obsm["spatial"])
     connectivities = adata.obsp["spatial_connectivities"]
-    values = adata.obs[domain_key].values
+    values = adata.obs[niche_key].values
 
     keep = (
         (connectivities[delaunay.simplices[:, 0], delaunay.simplices[:, 1]].A1 == 1)
@@ -38,7 +38,7 @@ def geometrize_domains(adata: AnnData, domain_key: str) -> gpd.GeoDataFrame:
         component.visit(simplices_to_visit)
 
         data["geometry"].append(component.polygon)
-        data[domain_key].append(values[component.first_vertex_index()])
+        data[niche_key].append(values[component.first_vertex_index()])
 
     gdf = gpd.GeoDataFrame(data)
 
@@ -49,23 +49,23 @@ def geometrize_domains(adata: AnnData, domain_key: str) -> gpd.GeoDataFrame:
     return gdf
 
 
-def domains_geometry_stats(
+def niches_geometry_stats(
     adata: AnnData,
-    domain_key: str,
+    niche_key: str,
     aggregation: str | list[str] = "min",
-    key_added_suffix: str = "_distance_to_domain_",
+    key_added_suffix: str = "_distance_to_niche_",
 ) -> gpd.GeoDataFrame:
-    gdf = geometrize_domains(adata, domain_key)
+    gdf = geometrize_niches(adata, niche_key)
 
     pairwise_distances: pd.DataFrame = gdf.geometry.apply(lambda g: gdf.distance(g))
-    pairwise_distances[domain_key] = gdf[domain_key]
+    pairwise_distances[niche_key] = gdf[niche_key]
 
     if isinstance(aggregation, str):
         aggregation = [aggregation]
 
     for aggr in aggregation:
-        df = pairwise_distances.groupby(domain_key).aggregate(aggr).T
+        df = pairwise_distances.groupby(niche_key).aggregate(aggr).T
         df.columns = [f"{aggr}{key_added_suffix}{c}" for c in df.columns]
         gdf[df.columns] = df
 
-    return gdf.groupby(domain_key)[gdf.columns[2:]].mean()
+    return gdf.groupby(niche_key)[gdf.columns[2:]].mean()
