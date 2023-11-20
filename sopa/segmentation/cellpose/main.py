41c8db7faa2906c90db2ff32f43f1e4257e05840
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2023-11-20 09:47:04 +0100

    fix issue len channels cellpose

--- sopa/segmentation/cellpose/main.py
+++ sopa/segmentation/cellpose/main.py
@@ -6,7 +6,7 @@ def cellpose_patch(
     diameter: float, channels: list[int], model_type: str = "cyto2", **cellpose_kwargs
 ):
     model = models.Cellpose(model_type=model_type)
-    channels = list(range(channels))
+    channels = list(range(len(channels)))
 
     def _(patch: np.ndarray):
         mask, *_ = model.eval(patch, diameter=diameter, channels=channels, **cellpose_kwargs)
