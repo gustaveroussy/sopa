87965d0111be9a146696e00b9a3232481944eb7f
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2023-11-20 09:53:13 +0100

    try better channel input cellpose

--- sopa/segmentation/cellpose/main.py
+++ sopa/segmentation/cellpose/main.py
@@ -3,10 +3,16 @@ from cellpose import models
 
 
 def cellpose_patch(
-    diameter: float, channels: list[int], model_type: str = "cyto2", **cellpose_kwargs
+    diameter: float, channels: list[str], model_type: str = "cyto2", **cellpose_kwargs
 ):
     model = models.Cellpose(model_type=model_type)
-    channels = list(range(len(channels)))
+
+    if isinstance(channels, str) or len(channels) == 1:
+        channels = [0, 0]  # gray scale
+    elif len(channels) == 2:
+        channels = [1, 2]
+    else:
+        raise ValueError(f"Provide 1 or 2 channels. Found {len(channels)}")
 
     def _(patch: np.ndarray):
         mask, *_ = model.eval(patch, diameter=diameter, channels=channels, **cellpose_kwargs)
