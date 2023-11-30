1f949ccb690c3edf58375a195c85c22c7932f044
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2023-11-30 18:12:10 +0100

    imread macsima more flexible

--- sopa/io/imaging.py
+++ sopa/io/imaging.py
@@ -22,7 +22,7 @@ def _deduplicate_names(df):
 
 
 def _parse_name_macsima(file):
-    index = file.name[2:5]
+    index = file.name[2:5] if file.name[0] == "C" else file.name[:3]
     match = re.search(r"_A-(.*?)_C-", file.name)
     if match:
         antibody = match.group(1)
