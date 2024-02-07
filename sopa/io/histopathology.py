f67ee1a197c2b41541345592be2ea374c0a12172
Christodoulidis Stergios <stergios.christodoulidis@centralesupelec.fr>
2024-02-08 00:46:55 +0100

    read_wsi now returns SpatialData obj

--- sopa/io/histopathology.py
+++ sopa/io/histopathology.py
@@ -3,23 +3,45 @@ import tiffslide
 import dask.array as da
 from pathlib import Path
 from xarray import DataArray
+from spatialdata import SpatialData
+from spatial_image import SpatialImage
+from spatialdata.models import Image2DModel
+from spatialdata.transformations import Identity
 
-def read_wsi(path: Path) -> DataArray:
+def read_wsi(path: Path) -> SpatialData:
     image_name = Path(path).absolute().name.split(".")[0]
     tiff = tiffslide.open_slide(path)
-    image = xarray.open_zarr(
+    img = xarray.open_zarr(
         tiff.zarr_group.store,
         consolidated=False,
         mask_and_scale=False,
     )
 
-    image.attrs['properties'] = tiff.properties
-    image.attrs['dimensions'] = tiff.dimensions
-    image.attrs['level_count'] = tiff.level_count
-    image.attrs['level_dimensions'] = tiff.level_dimensions
-    image.attrs['level_downsamples'] = tiff.level_downsamples
+    tiff_metadata = {
+        'properties': tiff.properties,
+        'dimensions': tiff.dimensions,
+        'level_count': tiff.level_count,
+        'level_dimensions': tiff.level_dimensions,
+        'level_downsamples': tiff.level_downsamples
+    }
 
-    return image
+    images = {}
+    for k in list(img.keys()):
+        ap = k if k!='0' else ''
+        
+        simg = SpatialImage(
+            img[k].transpose("S","Y"+ap,"X"+ap),
+            dims=('c','y','x'),
+            attrs={'metadata': tiff_metadata}
+        )
+
+        images[k] = Image2DModel.parse(
+            simg,
+            transformations={"pixels": Identity()},
+            c_coords=('r','g','b')
+        )
+
+    return SpatialData(images=images)
 
 if __name__ == '__main__':
     # test file here: https://openslide.cs.cmu.edu/download/openslide-testdata/Hamamatsu/CMU-1.ndpi
