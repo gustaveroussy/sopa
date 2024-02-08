6ea366e224ef8894f1150f45ce178d9fd89e3dc4
Christodoulidis Stergios <stergios.christodoulidis@centralesupelec.fr>
2024-02-08 01:01:13 +0100

    isort and black

--- sopa/io/histopathology.py
+++ sopa/io/histopathology.py
@@ -1,12 +1,14 @@
-import xarray
-import tiffslide
-import dask.array as da
 from pathlib import Path
-from xarray import DataArray
-from spatialdata import SpatialData
+
+import dask.array as da
+import tiffslide
+import xarray
 from spatial_image import SpatialImage
+from spatialdata import SpatialData
 from spatialdata.models import Image2DModel
 from spatialdata.transformations import Identity
+from xarray import DataArray
+
 
 def read_wsi(path: Path) -> SpatialData:
     image_name = Path(path).absolute().name.split(".")[0]
@@ -18,32 +20,31 @@ def read_wsi(path: Path) -> SpatialData:
     )
 
     tiff_metadata = {
-        'properties': tiff.properties,
-        'dimensions': tiff.dimensions,
-        'level_count': tiff.level_count,
-        'level_dimensions': tiff.level_dimensions,
-        'level_downsamples': tiff.level_downsamples
+        "properties": tiff.properties,
+        "dimensions": tiff.dimensions,
+        "level_count": tiff.level_count,
+        "level_dimensions": tiff.level_dimensions,
+        "level_downsamples": tiff.level_downsamples,
     }
 
     images = {}
     for k in list(img.keys()):
-        ap = k if k!='0' else ''
-        
+        ap = k if k != "0" else ""
+
         simg = SpatialImage(
-            img[k].transpose("S","Y"+ap,"X"+ap),
-            dims=('c','y','x'),
-            attrs={'metadata': tiff_metadata}
+            img[k].transpose("S", "Y" + ap, "X" + ap),
+            dims=("c", "y", "x"),
+            attrs={"metadata": tiff_metadata},
         )
 
         images[k] = Image2DModel.parse(
-            simg,
-            transformations={"pixels": Identity()},
-            c_coords=('r','g','b')
+            simg, transformations={"pixels": Identity()}, c_coords=("r", "g", "b")
         )
 
     return SpatialData(images=images)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # test file here: https://openslide.cs.cmu.edu/download/openslide-testdata/Hamamatsu/CMU-1.ndpi
-    img = read_wsi('CMU-1.ndpi')
+    img = read_wsi("CMU-1.ndpi")
     print(img)
--- sopa/segmentation/tissue.py
+++ sopa/segmentation/tissue.py
@@ -1,42 +1,38 @@
 import cv2
-import numpy as np
 import geopandas as gpd
-from sopa.io import read_wsi
-from xarray import DataArray
-from spatialdata import SpatialData
+import numpy as np
 from shapely.geometry import Polygon
+from spatialdata import SpatialData
 from spatialdata.models import ShapesModel
 from spatialdata.transformations import get_transformation
+from xarray import DataArray
+
+from sopa.io import read_wsi
+
 
 def hsv_otsu(
     sdata: SpatialData,
-    lvl: int=-1,
-    blur_k: int=5, 
-    open_k: int=5, 
-    close_k: int=5, 
-    drop_threshold: int=0.01
+    lvl: int = -1,
+    blur_k: int = 5,
+    open_k: int = 5,
+    close_k: int = 5,
+    drop_threshold: int = 0.01,
 ):
 
     level_keys = list(sdata.images.keys())
     image = sdata[level_keys[lvl]]
 
-    thumbnail = np.array(image.transpose('y','x','c'))
+    thumbnail = np.array(image.transpose("y", "x", "c"))
     thumbnail_hsv = cv2.cvtColor(thumbnail, cv2.COLOR_RGB2HSV)
     thumbnail_hsv_blurred = cv2.medianBlur(thumbnail_hsv[:, :, 1], blur_k)
-    _, mask = cv2.threshold(
-        thumbnail_hsv_blurred, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY
-    )
+    _, mask = cv2.threshold(thumbnail_hsv_blurred, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY)
 
-    mask_open = cv2.morphologyEx(
-        mask, cv2.MORPH_OPEN, np.ones((open_k, open_k), np.uint8)
-    )
+    mask_open = cv2.morphologyEx(mask, cv2.MORPH_OPEN, np.ones((open_k, open_k), np.uint8))
     mask_open_close = cv2.morphologyEx(
         mask_open, cv2.MORPH_CLOSE, np.ones((close_k, close_k), np.uint8)
     )
 
-    num_labels, labels, stats, _ = cv2.connectedComponentsWithStats(
-        mask_open_close, 4, cv2.CV_32S
-    )
+    num_labels, labels, stats, _ = cv2.connectedComponentsWithStats(mask_open_close, 4, cv2.CV_32S)
 
     contours = []
     for i in range(1, num_labels):
@@ -52,16 +48,14 @@ def hsv_otsu(
     polygons = [Polygon(contour) for contour in contours]
     geo_df = gpd.GeoDataFrame(geometry=polygons)
 
-    geo_df = ShapesModel.parse(
-        geo_df,
-        transformations=get_transformation(image, get_all=True)
-    )
-    sdata.add_shapes('tissue', geo_df)
+    geo_df = ShapesModel.parse(geo_df, transformations=get_transformation(image, get_all=True))
+    sdata.add_shapes("tissue", geo_df)
 
-if __name__ == '__main__':
-    img = read_wsi('CMU-1.ndpi')
+
+if __name__ == "__main__":
+    img = read_wsi("CMU-1.ndpi")
     hsv_otsu(img)
-    
+
     # import spatialdata_plot
     # img\
     #     .pl.render_images('4')\
