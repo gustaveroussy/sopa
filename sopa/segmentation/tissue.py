8b8450ce74981067f78569c875258e182bb9e8bf
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2024-02-09 10:58:04 +0100

    cleanup tissue segmentation

--- sopa/segmentation/tissue.py
+++ sopa/segmentation/tissue.py
@@ -1,24 +1,50 @@
-import cv2
+import logging
+
 import geopandas as gpd
 import numpy as np
+import xarray as xr
 from shapely.geometry import Polygon
 from spatialdata import SpatialData
 from spatialdata.models import ShapesModel
 
+from .._constants import ROI
+from .._sdata import get_key
+
+log = logging.getLogger(__name__)
+
 
 def hsv_otsu(
     sdata: SpatialData,
-    element_name: str,
-    lvl: int = -1,
+    image_key: str | None = None,
+    level: int = -1,
     blur_k: int = 5,
     open_k: int = 5,
     close_k: int = 5,
     drop_threshold: int = 0.01,
 ):
-    level_keys = list(sdata[element_name].keys())
-    image = sdata[element_name][level_keys[lvl]]
+    """Perform WSI tissue segmentation. The resulting ROI are saved as shapes.
 
-    thumbnail = np.array(image["image"].transpose("y", "x", "c"))
+    Args:
+        sdata: A `SpatialData` object representing an H&E image
+        image_key: Optional key of the H&E image
+        level: Level of the multiscale image on which
+        blur_k: TODO@stergios
+        open_k: TODO@stergios
+        close_k: TODO@stergios
+        drop_threshold: TODO@stergios
+    """
+    import cv2
+
+    assert (
+        ROI.KEY not in sdata.shapes
+    ), f"sdata['{ROI.KEY}'] was already existing, but tissue segmentation is run on top. Delete the shape(s) first."
+
+    image_key = get_key(sdata, "images", image_key)
+
+    level_keys = list(sdata[image_key].keys())
+    image: xr.DataArray = next(iter(sdata[image_key][level_keys[level]].values()))
+
+    thumbnail = np.array(image.transpose("y", "x", "c"))
     thumbnail_hsv = cv2.cvtColor(thumbnail, cv2.COLOR_RGB2HSV)
     thumbnail_hsv_blurred = cv2.medianBlur(thumbnail_hsv[:, :, 1], blur_k)
     _, mask = cv2.threshold(thumbnail_hsv_blurred, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY)
@@ -41,24 +67,12 @@ def hsv_otsu(
             c_closed = np.array(list(cc) + [cc[0]])
             contours.extend([c_closed.squeeze()])
 
-    polygons = [Polygon(contour) for contour in contours]
-    geo_df = gpd.GeoDataFrame(geometry=polygons)
-
+    geo_df = gpd.GeoDataFrame(geometry=[Polygon(contour) for contour in contours])
     geo_df = ShapesModel.parse(
         geo_df,
-        transformations=sdata.images[element_name][level_keys[lvl]]["image"].attrs["transform"],
+        transformations=image.attrs["transform"],
     )
-    sdata.add_shapes("tissue", geo_df)
 
+    sdata.add_shapes(ROI.KEY, geo_df)
 
-if __name__ == "__main__":
-    from sopa.io import read_wsi
-    
-    img = read_wsi("CMU-1.ndpi")
-    hsv_otsu(img, "CMU-1")
-
-    import spatialdata_plot
-
-    img.pl.render_images("CMU-1", scale="scale3").pl.render_shapes(
-        outline_color="green", fill_alpha=0, outline=True
-    ).pl.show(dpi=300, save="CMU-1-tissue_segmentation.png")
+    log.info(f"Tissue segmentation saved in sdata['{ROI.KEY}']")
