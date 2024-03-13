40865c19616cf1d365bfd08e2233cf14c1b474d3
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2024-03-13 15:12:10 +0100

    experimental rioxarray backend

--- sopa/io/transcriptomics.py
+++ sopa/io/transcriptomics.py
@@ -5,26 +5,28 @@
 from __future__ import annotations
 
 import json
+import logging
 import re
+import warnings
 from collections.abc import Mapping
 from pathlib import Path
 from types import MappingProxyType
 from typing import Any
 
-import anndata
 import dask.dataframe as dd
-import geopandas
 import numpy as np
-import pandas as pd
 import spatialdata_io
+import xarray
 from dask import array as da
 from dask.dataframe import read_parquet
 from dask_image.imread import imread
 from spatialdata import SpatialData
 from spatialdata._logging import logger
-from spatialdata.models import Image2DModel, PointsModel, ShapesModel, TableModel
+from spatialdata.models import Image2DModel, PointsModel
 from spatialdata.transformations import Affine, Identity, Scale
 from spatialdata_io._constants._constants import MerscopeKeys, XeniumKeys
+
+log = logging.getLogger(__name__)
 
 
 def _get_channel_names(images_dir: Path) -> list[str]:
@@ -36,58 +38,15 @@ def _get_channel_names(images_dir: Path) -> list[str]:
     return list(stainings)
 
 
-def _get_file_paths(
-    path: Path, vpt_outputs: Path | str | dict[str, Any] | None
-) -> tuple[Path, Path, Path]:
-    """
-    Gets the MERSCOPE file paths when vpt_outputs is provided
-
-    That is, (i) the file of transcript per cell, (ii) the cell metadata file, and (iii) the cell boundary file
-    """
-    if vpt_outputs is None:
-        return (
-            path / MerscopeKeys.COUNTS_FILE,
-            path / MerscopeKeys.CELL_METADATA_FILE,
-            path / MerscopeKeys.BOUNDARIES_FILE,
-        )
-
-    if isinstance(vpt_outputs, str) or isinstance(vpt_outputs, Path):
-        vpt_outputs = Path(vpt_outputs)
-
-        plausible_boundaries = [
-            vpt_outputs / MerscopeKeys.CELLPOSE_BOUNDARIES,
-            vpt_outputs / MerscopeKeys.WATERSHED_BOUNDARIES,
-        ]
-        valid_boundaries = [path for path in plausible_boundaries if path.exists()]
-
-        assert (
-            valid_boundaries
-        ), f"Boundary file not found - expected to find one of these files: {', '.join(map(str, plausible_boundaries))}"
-
-        return (
-            vpt_outputs / MerscopeKeys.COUNTS_FILE,
-            vpt_outputs / MerscopeKeys.CELL_METADATA_FILE,
-            valid_boundaries[0],
-        )
-
-    if isinstance(vpt_outputs, dict):
-        return (
-            vpt_outputs[MerscopeKeys.VPT_NAME_COUNTS],
-            vpt_outputs[MerscopeKeys.VPT_NAME_OBS],
-            vpt_outputs[MerscopeKeys.VPT_NAME_BOUNDARIES],
-        )
-
-    raise ValueError(
-        f"`vpt_outputs` has to be either `None`, `str`, `Path`, or `dict`. Found type {type(vpt_outputs)}."
-    )
+SUPPORTED_BACKENDS = ["dask_image", "rioxarray"]
 
 
 def merscope(
     path: str | Path,
-    vpt_outputs: Path | str | dict[str, Any] | None = None,
     z_layers: int | list[int] | None = 3,
     region_name: str | None = None,
     slide_name: str | None = None,
+    backend: str = "dask_image",
     imread_kwargs: Mapping[str, Any] = MappingProxyType({}),
     image_models_kwargs: Mapping[str, Any] = MappingProxyType({}),
 ) -> SpatialData:
@@ -95,11 +54,19 @@ def merscope(
 
     Args:
         path: Path to the MERSCOPE directory containing all the experiment files
+        backend: Either `dask_image` or `rioxarray` (the latter should use less RAM, but it is still experimental)
         **kwargs: See link above.
 
     Returns:
         A `SpatialData` object representing the MERSCOPE experiment
     """
+    assert (
+        backend in SUPPORTED_BACKENDS
+    ), f"Backend '{backend} not supported. Should be one of: {', '.join(SUPPORTED_BACKENDS)}"
+
+    if backend == "rioxarray":
+        log.info("Using experimental rioxarray backend.")
+
     if "chunks" not in image_models_kwargs:
         if isinstance(image_models_kwargs, MappingProxyType):
             image_models_kwargs = {}
@@ -112,7 +79,6 @@ def merscope(
         image_models_kwargs["scale_factors"] = [2, 2, 2, 2]
 
     path = Path(path).absolute()
-    count_path, obs_path, boundaries_path = _get_file_paths(path, vpt_outputs)
     images_dir = path / MerscopeKeys.IMAGES_DIR
 
     microns_to_pixels = Affine(
@@ -124,7 +90,6 @@ def merscope(
     vizgen_region = path.name if region_name is None else region_name
     slide_name = path.parent.name if slide_name is None else slide_name
     dataset_id = f"{slide_name}_{vizgen_region}"
-    region = f"{dataset_id}_polygons"
 
     # Images
     images = {}
@@ -132,23 +97,19 @@ def merscope(
     z_layers = [z_layers] if isinstance(z_layers, int) else z_layers or []
 
     stainings = _get_channel_names(images_dir)
+    image_transformations = {"microns": microns_to_pixels.inverse()}
+    reader = _rioxarray_load_merscope if backend == "rioxarray" else _dask_image_load_merscope
+
     if stainings:
         for z_layer in z_layers:
-            im = da.stack(
-                [
-                    imread(images_dir / f"mosaic_{stain}_z{z_layer}.tif", **imread_kwargs).squeeze()
-                    for stain in stainings
-                ],
-                axis=0,
+            images[f"{dataset_id}_z{z_layer}"] = reader(
+                images_dir,
+                stainings,
+                z_layer,
+                image_models_kwargs,
+                image_transformations,
+                **imread_kwargs,
             )
-            parsed_im = Image2DModel.parse(
-                im,
-                dims=("c", "y", "x"),
-                transformations={"microns": microns_to_pixels.inverse()},
-                c_coords=stainings,
-                **image_models_kwargs,
-            )
-            images[f"{dataset_id}_z{z_layer}"] = parsed_im
 
     # Transcripts
     points = {}
@@ -160,25 +121,60 @@ def merscope(
             f"Transcript file {transcript_path} does not exist. Transcripts are not loaded."
         )
 
-    # Polygons
-    shapes = {}
-    if boundaries_path.exists():
-        shapes[f"{dataset_id}_polygons"] = _get_polygons(boundaries_path)
-    else:
-        logger.warning(
-            f"Boundary file {boundaries_path} does not exist. Cell boundaries are not loaded."
-        )
+    return SpatialData(points=points, images=images)
 
-    # Table
-    table = None
-    if count_path.exists() and obs_path.exists():
-        table = _get_table(count_path, obs_path, vizgen_region, slide_name, dataset_id, region)
-    else:
-        logger.warning(
-            f"At least one of the following files does not exist: {count_path}, {obs_path}. The table is not loaded."
-        )
 
-    return SpatialData(shapes=shapes, points=points, images=images, table=table)
+def _rioxarray_load_merscope(
+    images_dir: Path,
+    stainings: list[str],
+    z_layer: int,
+    image_models_kwargs: dict,
+    transformations: dict,
+    **kwargs,
+):
+    import rioxarray
+    from rasterio.errors import NotGeoreferencedWarning
+
+    warnings.simplefilter("ignore", category=NotGeoreferencedWarning)
+
+    im = xarray.concat(
+        [
+            rioxarray.open_rasterio(
+                images_dir / f"mosaic_{stain}_z{z_layer}.tif",
+                chunks=image_models_kwargs["chunks"],
+                **kwargs,
+            ).rename({"band": "c"})
+            for stain in stainings
+        ],
+        dim="c",
+    )
+
+    return Image2DModel.parse(im, transformations=transformations, **image_models_kwargs)
+
+
+def _dask_image_load_merscope(
+    images_dir: Path,
+    stainings: list[str],
+    z_layer: int,
+    image_models_kwargs: dict,
+    transformations: dict,
+    **kwargs,
+):
+    im = da.stack(
+        [
+            imread(images_dir / f"mosaic_{stain}_z{z_layer}.tif", **kwargs).squeeze()
+            for stain in stainings
+        ],
+        axis=0,
+    )
+
+    return Image2DModel.parse(
+        im,
+        dims=("c", "y", "x"),
+        transformations=transformations,
+        c_coords=stainings,
+        **image_models_kwargs,
+    )
 
 
 def _get_points(transcript_path: Path):
@@ -190,49 +186,6 @@ def _get_points(transcript_path: Path):
     )
     transcripts["gene"] = transcripts["gene"].astype("category")
     return transcripts
-
-
-def _get_polygons(boundaries_path: Path) -> geopandas.GeoDataFrame:
-    geo_df = geopandas.read_parquet(boundaries_path)
-    geo_df = geo_df.rename_geometry("geometry")
-    geo_df = geo_df[geo_df[MerscopeKeys.Z_INDEX] == 0]  # Avoid duplicate boundaries on all z-levels
-    geo_df.geometry = geo_df.geometry.map(
-        lambda x: x.geoms[0]
-    )  # The MultiPolygons contain only one polygon
-    geo_df.index = geo_df[MerscopeKeys.METADATA_CELL_KEY].astype(str)
-
-    return ShapesModel.parse(geo_df, transformations={"microns": Identity()})
-
-
-def _get_table(
-    count_path: Path,
-    obs_path: Path,
-    vizgen_region: str,
-    slide_name: str,
-    dataset_id: str,
-    region: str,
-) -> anndata.AnnData:
-    data = pd.read_csv(count_path, index_col=0, dtype={MerscopeKeys.COUNTS_CELL_KEY: str})
-    obs = pd.read_csv(obs_path, index_col=0, dtype={MerscopeKeys.METADATA_CELL_KEY: str})
-
-    is_gene = ~data.columns.str.lower().str.contains("blank")
-    adata = anndata.AnnData(data.loc[:, is_gene], dtype=data.values.dtype, obs=obs)
-
-    adata.obsm["blank"] = data.loc[:, ~is_gene]  # blank fields are excluded from adata.X
-    adata.obsm["spatial"] = adata.obs[[MerscopeKeys.CELL_X, MerscopeKeys.CELL_Y]].values
-    adata.obs["region"] = pd.Series(vizgen_region, index=adata.obs_names, dtype="category")
-    adata.obs["slide"] = pd.Series(slide_name, index=adata.obs_names, dtype="category")
-    adata.obs["dataset_id"] = pd.Series(dataset_id, index=adata.obs_names, dtype="category")
-    adata.obs[MerscopeKeys.REGION_KEY] = pd.Series(region, index=adata.obs_names, dtype="category")
-    adata.obs[MerscopeKeys.METADATA_CELL_KEY] = adata.obs.index
-
-    table = TableModel.parse(
-        adata,
-        region_key=MerscopeKeys.REGION_KEY.value,
-        region=region,
-        instance_key=MerscopeKeys.METADATA_CELL_KEY.value,
-    )
-    return table
 
 
 def xenium(
