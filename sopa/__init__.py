4f9189ac5346a8d7700025790a4eea45717a468d
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2024-08-11 13:27:55 +0200

    add submodule imports in init

--- sopa/__init__.py
+++ sopa/__init__.py
@@ -1,8 +1,14 @@
 import importlib.metadata
 import logging
+import sys
 from ._logging import configure_logger
 
 __version__ = importlib.metadata.version("sopa")
 
 log = logging.getLogger("sopa")
 configure_logger(log)
+
+if "--help" not in sys.argv:
+    from . import utils
+    from . import io
+    from . import segmentation
