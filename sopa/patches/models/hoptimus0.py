0637d3d0ffaf2fa2c89bf86af33c00d804f3ff73
Christodoulidis Stergios <stergios.christodoulidis@centralesupelec.fr>
2025-02-10 20:43:16 +0100

    Oops, I forgot the pre-commit

--- sopa/patches/models/hoptimus0.py
+++ sopa/patches/models/hoptimus0.py
@@ -9,11 +9,10 @@ class HOPTIMUSFeatures(nn.Module):
         self.model = timm.create_model("hf_hub:bioptimus/H-optimus-0", pretrained=True)
 
     def forward(self, x):
-        transform = transforms.Compose([
-            transforms.Normalize(
-                mean=(0.707223, 0.578729, 0.703617), 
-                std=(0.211883, 0.230117, 0.177517)
-            ),
-        ])
+        transform = transforms.Compose(
+            [
+                transforms.Normalize(mean=(0.707223, 0.578729, 0.703617), std=(0.211883, 0.230117, 0.177517)),
+            ]
+        )
 
         return self.model(transform(x))
