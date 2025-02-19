54d9d13b83290623c1bb211b097028d1998cf5d3
Quentin Blampey <quentin.blampey@gmail.com>
2025-02-19 17:44:58 +0100

    fix embedding_config typo

--- sopa/patches/infer.py
+++ sopa/patches/infer.py
@@ -90,7 +90,7 @@ def compute_embeddings(
         instance_key="instance",
     )
     adata.obsm["spatial"] = gdf.centroid.get_coordinates().values
-    adata.uns["emebedding_config"] = {
+    adata.uns["embedding_config"] = {
         "patch_width": patch_width,
         "patch_overlap": patch_overlap,
         "magnification": magnification,
