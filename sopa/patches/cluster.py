1c379cbcccbf48bf6bfe9c198942e630b4cb0707
Christodoulidis Stergios <stergios.christodoulidis@centralesupelec.fr>
2025-02-19 17:11:50 +0100

    adds kmeans as a clustering method performed directly on the `AnnData` object

--- sopa/patches/cluster.py
+++ sopa/patches/cluster.py
@@ -13,8 +13,16 @@ def leiden_clustering(adata: AnnData, flavor: str = "igraph", **kwargs):
     return adata.obs["leiden"].values
 
 
+def kmeans_clustering(adata: AnnData, **kwargs):
+    from sklearn.cluster import KMeans
+
+    kmeans = KMeans(**kwargs)
+    return kmeans.fit_predict(adata.X)
+
+
 METHODS_DICT = {
     "leiden": leiden_clustering,
+    "kmeans": kmeans_clustering,
 }
 
 
@@ -33,7 +41,7 @@ def cluster_embeddings(
     Args:
         sdata: A `SpatialData` object. Can be `None` if element is an `AnnData` object.
         element: The `AnnData` containing the embeddings, or the name of the element
-        method: Callable that takes as an AnnData object and returns an array of clusters of size `n_obs`, or an available method name (`leiden`)
+        method: Callable that takes as an AnnData object and returns an array of clusters of size `n_obs`, or an available method name (`leiden` or `kmeans`)
         key_added: The key containing the clusters to be added to the `element.obs`
         method_kwargs: kwargs provided to the method callable
     """
@@ -44,5 +52,5 @@ def cluster_embeddings(
         assert method in METHODS_DICT, f"Method {method} is not available. Use one of: {', '.join(METHODS_DICT.keys())}"
         method = METHODS_DICT[method]
 
-    element.obs[key_added] = method(element.X, **method_kwargs)
+    element.obs[key_added] = method(element, **method_kwargs)
     element.obs[key_added] = element.obs[key_added].astype("category")
