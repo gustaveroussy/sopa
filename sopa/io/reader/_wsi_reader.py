0856a2c8ae2f77c0521833f37e5ca493872be5dc
Christodoulidis Stergios <stergios.christodoulidis@centralesupelec.fr>
2025-06-05 16:37:50 +0200

    minor fixes

--- sopa/io/reader/_wsi_reader.py
+++ sopa/io/reader/_wsi_reader.py
@@ -78,7 +78,7 @@ class OpenSlideReader(ReaderBase):
 
         return KVStore(WsiStore(self, tilesize))
 
-    def close():
+    def close(self):
         """Close the slide."""
         self.slide.close()
 
@@ -122,7 +122,7 @@ class TiffSlideReader(ReaderBase):
     def get_zarr_store(self, tilesize: int = 512):
         return self.slide.zarr_group.store
 
-    def close():
+    def close(self):
         """Close the slide."""
         self.slide.close()
 
@@ -165,14 +165,14 @@ class SlideIOReader(ReaderBase):
         
         with self.slide.get_scene(0) as scene:
             scaling = 1 / scene.get_zoom_level_info(level).scale
-            (x_start, y_start) = location
-            x_end = min(x_start + int(scaling * size[0]), scene.size[0])
-            y_end = min(y_start + int(scaling * size[1]), scene.size[1])
-            x_width = x_end - x_start
-            y_height = y_end - y_start
+            (x, y) = location
+            x_end = min(x + int(scaling * size[0]), scene.size[0])
+            y_end = min(y + int(scaling * size[1]), scene.size[1])
+            x_width = x_end - x
+            y_height = y_end - y
             tile_x = int(np.round(x_width / scaling))
             tile_y = int(np.round(y_height / scaling))
-            _tile = scene.read_block((x_start, y_start, x_width, y_height), (tile_x, tile_y))
+            _tile = scene.read_block((x, y, x_width, y_height), (tile_x, tile_y))
             tile = np.zeros((size[0], size[1], scene.num_channels), dtype=np.uint8)
             tile[: _tile.shape[0], : _tile.shape[1], :] = _tile
         return np.array(tile)
@@ -183,13 +183,11 @@ class SlideIOReader(ReaderBase):
 
         return KVStore(WsiStore(self, tilesize))
 
-    def close():
-        """Close the slide."""
-        self.slide.close()
+    def close(self):
+        pass
 
     @property
     def properties(self):
-        """Get the properties of the slide."""
         return {"slideio.objective-power": self.slide.get_scene(0).magnification}
 
     @property
