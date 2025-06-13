48a97c403cfa9e7b1780dd8167a2abc71b497e9f
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2025-06-13 15:06:32 +0200

    add back aggregator in init

--- sopa/aggregation/__init__.py
+++ sopa/aggregation/__init__.py
@@ -1,5 +1,5 @@
 from .bins import aggregate_bins
 from .channels import aggregate_channels
 from .transcripts import count_transcripts
-from .aggregation import aggregate
+from .aggregation import aggregate, Aggregator
 from .overlay import overlay_segmentation
