449f8ef89e54b4bc61fd7e128b090bb764bb9248
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2025-10-24 13:33:46 +0200

    add wsi_backends pytest mark for optional dependencies

--- tests/test_wsi_backends.py
+++ tests/test_wsi_backends.py
@@ -1,13 +1,18 @@
+import pytest
+
 import sopa
 
 
+@pytest.mark.wsi_backends
 def test_tiffslide_backend():
     sopa.io.wsi("tests/CMU-1-Small-Region.svs", backend="tiffslide")
 
 
+@pytest.mark.wsi_backends
 def test_openslide_backend():
     sopa.io.wsi("tests/CMU-1-Small-Region.svs", backend="openslide")
 
 
+@pytest.mark.wsi_backends
 def test_slideio_backend():
     sopa.io.wsi("tests/CMU-1-Small-Region.svs", backend="slideio")
