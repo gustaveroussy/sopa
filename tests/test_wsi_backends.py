fefd9546b332a40eb99922c9601d9aff8217aa65
Christodoulidis Stergios <stergios.christodoulidis@centralesupelec.fr>
2025-10-24 14:05:41 +0200

    adds a test to check if the region is the same for the different backends

--- tests/test_wsi_backends.py
+++ tests/test_wsi_backends.py
@@ -16,3 +16,29 @@ def test_openslide_backend():
 @pytest.mark.wsi_backends
 def test_slideio_backend():
     sopa.io.wsi("tests/CMU-1-Small-Region.svs", backend="slideio")
+
+
+@pytest.mark.wsi_backends
+def test_region_matching():
+    import numpy as np
+    from sopa.io.reader._wsi_reader import get_reader
+
+    wsi_tiffslide = get_reader("tiffslide")("tests/CMU-1-Small-Region.svs")
+    wsi_openslide = get_reader("openslide")("tests/CMU-1-Small-Region.svs")
+    wsi_slideio = get_reader("slideio")("tests/CMU-1-Small-Region.svs")
+    wsi_xarray = get_reader("xarray")(sopa.io.wsi("tests/CMU-1-Small-Region.svs")["CMU-1-Small-Region"])
+
+    region_tiffslide = wsi_tiffslide.read_region((1000, 1000), level=0, size=(512, 512))
+    region_openslide = wsi_openslide.read_region((1000, 1000), level=0, size=(512, 512))
+    region_slideio = wsi_slideio.read_region((1000, 1000), level=0, size=(512, 512))
+    region_xarray = wsi_xarray.read_region((1000, 1000), level=0, size=(512, 512))
+
+    assert (np.array(region_tiffslide) == np.array(region_openslide)).all(), (
+        "Regions from tiffslide and openslide do not match"
+    )
+    assert (np.array(region_tiffslide) == np.array(region_slideio)).all(), (
+        "Regions from tiffslide and slideio do not match"
+    )
+    assert (np.array(region_tiffslide) == np.array(region_xarray)).all(), (
+        "Regions from tiffslide and xarray do not match"
+    )
