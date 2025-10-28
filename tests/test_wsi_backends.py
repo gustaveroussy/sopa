0beb54a421967ae576aabd1d97c9bffd2de77d2c
Blampey Quentin <quentin.blampey@centralesupelec.fr>
2025-10-28 14:57:30 +0100

    add test_deterministic_embedding

--- tests/test_wsi_backends.py
+++ tests/test_wsi_backends.py
@@ -1,4 +1,6 @@
+import numpy as np
 import pytest
+import spatialdata
 
 import sopa
 
@@ -46,3 +48,28 @@ def test_region_matching():
     assert (np.array(region_tiffslide) == np.array(region_xarray)).all(), (
         "Regions from tiffslide and xarray do not match"
     )
+
+
+@pytest.mark.wsi_backends
+@pytest.mark.parametrize("model", ["histo_ssl", "resnet50", "dinov2"])  # TODO: add conch and hoptimus0?
+def test_deterministic_embedding(model: str):
+    patch_width = 224
+
+    sdata_on_disk = sopa.io.wsi("tests/CMU-1-Small-Region.svs")
+    sdata_on_disk.write(f"tests/test_wsi_{model}.zarr")
+    sdata_on_disk = spatialdata.read_zarr(f"tests/test_wsi_{model}.zarr")
+    assert "backend" not in sdata_on_disk["CMU-1-Small-Region"].attrs  # fallback to xarray reader
+
+    sdata_tiffslide = sopa.io.wsi("tests/CMU-1-Small-Region.svs", backend="tiffslide")
+
+    sopa.patches.compute_embeddings(sdata_tiffslide, model, patch_width)
+
+    for other_backend in ["slideio", "openslide"]:
+        sdata_backend = sopa.io.wsi("tests/CMU-1-Small-Region.svs", backend=other_backend)
+        sopa.patches.compute_embeddings(sdata_backend, model, patch_width)
+
+        assert np.isclose(sdata_tiffslide[f"{model}_embeddings"].X, sdata_backend[f"{model}_embeddings"].X).all()
+
+    sopa.patches.compute_embeddings(sdata_on_disk, model, patch_width)
+
+    assert np.isclose(sdata_tiffslide[f"{model}_embeddings"].X, sdata_on_disk[f"{model}_embeddings"].X).all()
